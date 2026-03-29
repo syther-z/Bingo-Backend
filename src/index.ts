3b377c91bc407586792d3e60020ad2a091435f64
Pawan Kumar <pawansharma9115078358@gmail.com>
2026-03-29 10:24:02 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import http from 'http';
 import GameService from './service/GameService';
-import { BACKEND_URL, PORT } from './static';
-
+import { BACKEND_URL, FRONTEND_URL, PORT } from './static';
+import cors from 'cors';
 const app = express();
+app.use(cors({
+    origin: FRONTEND_URL
+}));
 const server = http.createServer(app);
 
 let gameService = new GameService(server);
